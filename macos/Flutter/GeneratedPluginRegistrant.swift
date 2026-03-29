2592b707acf8a4a62141d7e6807b07a7c67afab8
Romelianism <Romelianism@gmail.com>
2026-03-30 07:14:55 +0800

    feat(deps): add local notifications and background services
    Integrate flutter_local_notifications and flutter_background_service to enable push notification support and background task processing.
    
    This update includes the necessary plugin registrations for macOS and Windows, along with comprehensive documentation for platform-specific setup and configuration.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import cryptography_flutter
 import device_info_plus
 import flutter_ble_peripheral
 import flutter_blue_plus_darwin
+import flutter_local_notifications
 import share_plus
 import shared_preferences_foundation
 import sqlite3_flutter_libs
@@ -18,6 +19,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FlutterBlePeripheralPlugin.register(with: registry.registrar(forPlugin: "FlutterBlePeripheralPlugin"))
   FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
